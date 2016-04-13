LuisDev8913/phenomic

commit 1bfaa9e068e231302b957b3943851216d850651c

Internal: Improve PageContainer ref saving

src/PageContainer/component.js
@@ -209,6 +209,10 @@ class PageContainer extends Component<DefaultProps, Props, void> {
     }
   }
 
+  saveContentRef(ref: Element) {
+    this._content = ref
+  }
+
   render() {
     const { props, context } = this
 
@@ -259,7 +263,7 @@ class PageContainer extends Component<DefaultProps, Props, void> {
         }
         {
           !page.error && !page.loading && Layout &&
-          <Layout ref={ (ref) => this._content = ref } { ...page } />
+          <Layout ref={ this.saveContentRef } { ...page } />
         }
       </div>
     )

commit 531b8adecffc9dfcaa35e717c37aa37e04239664

Fix PageContainer regression
Introduced in 1bfaa9e068e231302b957b3943851216d850651c

src/PageContainer/component.js
@@ -209,9 +209,9 @@ class PageContainer extends Component<DefaultProps, Props, void> {
     }
   }
 
-  saveContentRef(ref: Element) {
+  saveContentRef: Function = (ref: Element): void => {
     this._content = ref
-  }
+  };
 
   render() {
     const { props, context } = this