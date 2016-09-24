LuisDev8913/phenomic

commit 34f3d4654cac7dc0f25d1fff583a64d9f49d5eb5

Upgrade Flow to 0.32.0 (#776)
* Fix Flow 0.32.0 annotations

* Avoid big if block

src/builder/webpack/config.common.js
@@ -20,11 +20,10 @@ export default (config: PhenomicConfig): WebpackConfig => {
 
   return {
     ...webpackConfig,
-    ...config.cache && hardSourceRecordsPath(cacheDir),
+    ...config.cache ? hardSourceRecordsPath(cacheDir) : {},
     plugins: [
       ...webpackConfig.plugins,
-      ...config.cache && hardSourcePlugin(cacheDir, config),
-
+      config.cache ? hardSourcePlugin(cacheDir, config) : {},
       new DefinePlugin({ "process.env": {
         NODE_ENV: wrap(
           config.production

flow/interfaces/phenomic.js
@@ -60,6 +60,7 @@ export type PhenomicConfig = {
   webpackConfig?: WebpackConfig,
   webpackConfigBrowser?: WebpackConfig,
   webpackConfigNode?: WebpackConfig,
+  cache?: boolean,
 }
 
 export type PhenomicLoaderOptions = {}

src/components/PageContainer/component.js
@@ -47,10 +47,10 @@ if (isClient()) {
 function find(
   collection: PhenomicCollection,
   pageUrl: string
-): Object {
+): ?PhenomicCollectionItem {
   return collection.find((item) => (
     item.__url === pageUrl ||
-    item.__url === pageUrl + "/"||
+    item.__url === pageUrl + "/" ||
     item.__resourceUrl === pageUrl
   ))
 }

src/loader-preset-default/index.js
@@ -7,11 +7,10 @@ import initHeadPropertyFromContent
 import initBodyPropertyFromContent
   from "../loader-plugin-init-body-property-from-content"
 
-export default [
-   // $FlowFixMe Missing annotation wtf?
+const plugins: Array<mixed> = [
   initHeadPropertyFromConfig,
-   // $FlowFixMe Missing annotation wtf?
   initHeadPropertyFromContent,
-   // $FlowFixMe Missing annotation wtf?
   initBodyPropertyFromContent,
 ]
+
+export default plugins

commit 80de95fa493ae82617a67d14e526c6951d65ab88

Fix static build regression introduced by #776
https://github.com/MoOx/phenomic/pull/776

poke @montogeek @thangngoc89

Actually this error was hidden by a webpack bug, see 9334b34

Now it’s working.

src/builder/webpack/config.common.js
@@ -23,7 +23,7 @@ export default (config: PhenomicConfig): WebpackConfig => {
     ...config.cache ? hardSourceRecordsPath(cacheDir) : {},
     plugins: [
       ...webpackConfig.plugins,
-      config.cache ? hardSourcePlugin(cacheDir, config) : {},
+      ...config.cache ? [ hardSourcePlugin(cacheDir, config) ] : [],
       new DefinePlugin({ "process.env": {
         NODE_ENV: wrap(
           config.production