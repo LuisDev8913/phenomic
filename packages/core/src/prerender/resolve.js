LuisDev8913/phenomic

commit c68633ad0457e244219e28836f58d7efcf3fcd58

fix(@phenomic/core): support static rendering of "by" parameters for main (first) collection
collection params in routes is now useless as we consider the first collection of the queries as the main query.

packages/core/src/prerender/resolve.js
@@ -2,16 +2,50 @@ import createQuery from "@phenomic/api-client/lib/query";
 
 const debug = require("debug")("phenomic:core:prerender:resolve");
 
+const arrayUnique = array => [...new Set(array)];
+
+const getRouteQueries = route => {
+  const initialRouteParams: Object = route.params || {};
+  if (!route.component.getQueries) {
+    debug(route.path, "have no queries");
+    return {};
+  }
+  return route.component.getQueries({
+    params: initialRouteParams
+  });
+};
+
+const getMainCollection = route => {
+  const keys = Object.keys(getRouteQueries(route));
+  const firstKey = keys[0];
+  const firstKeyAsInt = parseInt(keys[0], 10);
+  // parseInt("12.") == "12"
+  if (
+    // $FlowFixMe it's on purpose
+    firstKeyAsInt == firstKey &&
+    String(firstKeyAsInt).length == firstKey.length
+  ) {
+    console.warn(`The main collection used for ${route.path} is ${firstKey}`);
+  }
+  return firstKey;
+};
+
 const resolveURLsForDynamicParams = async function(
   phenomicFetch: PhenomicFetch,
   route: PhenomicRoute
 ) {
-  const collectionConfig = typeof route.collection === "string"
-    ? { collection: route.collection }
-    : route.collection;
+  // deprecate notice
+  // @todo remove for stable v1
+  if (route.collection) {
+    console.error(
+      `${route.path} have an attached parameter 'collection=$route.collection'.\n This parameter is now useless and can be safely removed`
+    );
+  }
+
+  const collectionConfig = { collection: getMainCollection(route) };
   debug("route", route.path);
-  if (!collectionConfig) {
-    debug("no valid collection", route.collection);
+  if (!collectionConfig.collection) {
+    debug("no valid collection detected for", route.path);
     return route;
   }
   // If the path doesn't contain any kind of parameter, no need to
@@ -26,14 +60,39 @@ const resolveURLsForDynamicParams = async function(
       : Object.keys(collectionConfig).join(",")}' for route '${route.path}'`
   );
   // @todo memoize for perfs and avoid uncessary call
+  const query = getRouteQueries(route);
+  debug(route.path, query);
+  const key =
+    (query[collectionConfig.collection] &&
+      query[collectionConfig.collection].by) ||
+    "key";
   const collection = await phenomicFetch(createQuery(collectionConfig));
-  debug(`collection fetched. ${collection.list.length} items`);
+  debug(route.path, `collection fetched. ${collection.list.length} items`);
   const path = route.path || "*";
-  return collection.list.map(item => {
+  const list = collection.list.reduce((acc, item) => {
+    if (!item[key]) {
+      return acc;
+    }
+    if (Array.isArray(item[key])) {
+      acc = acc.concat(item[key]);
+    } else {
+      acc.push(item[key]);
+    }
+    return acc;
+  }, []);
+  debug(route.path, "list (unique)", arrayUnique(list));
+  return arrayUnique(list).map(value => {
+    let resolvedPath = path.replace(":" + key, value);
+    let params = { [key]: value };
+    // try *
+    if (key === "key" && resolvedPath === path) {
+      resolvedPath = resolvedPath.replace("*", value);
+      params = { splat: value };
+    }
     return {
       ...route,
-      path: path.replace(/\*/, item.key),
-      params: { splat: item.key }
+      path: resolvedPath,
+      params
     };
   });
 };
@@ -81,15 +140,8 @@ const resolvePaginatedURLs = async function(
     debug(route.path, "is not paginated");
     return route.path;
   }
-  if (!route.component.getQueries) {
-    debug(route.path, "is paginated but have no queries");
-    return route.path;
-  }
-  const initialRouteParams: Object = route.params || {};
-  const initialRouteQuery = route.component.getQueries({
-    params: initialRouteParams
-  });
-  const query = findPaginatedQuery(initialRouteQuery);
+
+  const query = findPaginatedQuery(getRouteQueries(route));
   if (!query) {
     debug(route.path, "is paginated with queries, but no :after param found");
     return route.path;

docs/App.js
@@ -10,7 +10,7 @@ import Home from "./components/Home";
 import GettingStarted from "./components/GettingStarted";
 import DocPage from "./components/Page/Doc";
 import ShowcasePage from "./components/Page/Showcase";
-import ShowcaseList from "./components/ShowcaseList";
+import ShowcaseList, { ShowcaseListByTag } from "./components/ShowcaseList";
 import PageError from "./components/PageError";
 import NewsItem from "./components/News/NewsItem";
 import NewsList from "./components/News/NewsList";
@@ -20,13 +20,9 @@ const routes = () =>
     <Route component={Wrapper}>
       <Route path="/" component={Home} />
       <Route path="/docs/getting-started" component={GettingStarted} />
-      <Route path="/docs/*" component={DocPage} collection="docs" />
-      <Route path="/showcase" component={ShowcaseList} collection="showcase" />
-      <Route
-        path="/showcase/tag/:tag"
-        component={ShowcaseList}
-        collection="showcase"
-      />
+      <Route path="/docs/*" component={DocPage} />
+      <Route path="/showcase" component={ShowcaseList} />
+      <Route path="/showcase/tag/:showcaseTags" component={ShowcaseListByTag} />
       {/*
       <Route
         path="/showcase/after/:after"
@@ -35,19 +31,10 @@ const routes = () =>
         paginated
       />
       */}
-      <Route
-        path="/showcase/*"
-        component={ShowcasePage}
-        collection="showcase"
-      />
-      <Route path="/news" component={NewsList} collection="news" />
-      <Route
-        path="/news/after/:after"
-        component={NewsList}
-        collection="news"
-        paginated
-      />
-      <Route path="/news/*" component={NewsItem} collection="news" />
+      <Route path="/showcase/*" component={ShowcasePage} />
+      <Route path="/news" component={NewsList} />
+      <Route path="/news/after/:after" component={NewsList} paginated />
+      <Route path="/news/*" component={NewsItem} />
       <Route path="*" component={PageError} />
     </Route>
   </Router>;

docs/components/ShowcaseList/index.js
@@ -253,16 +253,19 @@ const styles = StyleSheet.create({
   */
 });
 
-export default createContainer(ShowcaseList, props => ({
+export const Component = ShowcaseList;
+
+export default createContainer(ShowcaseList, () => ({
+  showcase: query({
+    collection: "showcase"
+  })
+}));
+
+export const ShowcaseListByTag = createContainer(ShowcaseList, props => ({
   showcase: query({
     collection: "showcase",
-    // by: "showcaseTags",
-    ...(props.params.tag
-      ? {
-          by: "showcaseTags",
-          value: props.params.tag
-        }
-      : {})
+    by: "showcaseTags",
+    value: props.params.showcaseTags
     // order: "asc",
     // limit: 10,
     // after: props.params.after

commit 25ad9e4343db930201b438596b334b41a33bc8b2

@phenomic/core: Fix static rendering for collection items (when no by param is used)
Regression introduced in c68633ad0457e244219e28836f58d7efcf3fcd58

packages: @phenomic/core
tag: fix

Closes #1114

packages/core/src/prerender/resolve.js
@@ -2,6 +2,9 @@ import createQuery from "@phenomic/api-client/lib/query";
 
 const debug = require("debug")("phenomic:core:prerender:resolve");
 
+const defaultQueryKey = "default";
+const mainKey = "key";
+
 const arrayUnique = array => [...new Set(array)];
 
 const getRouteQueries = route => {
@@ -62,12 +65,18 @@ const resolveURLsForDynamicParams = async function(
   // @todo memoize for perfs and avoid uncessary call
   const query = getRouteQueries(route);
   debug(route.path, query);
-  const key =
+  let key =
     (query[collectionConfig.collection] &&
       query[collectionConfig.collection].by) ||
-    "key";
+    mainKey;
+  if (key === defaultQueryKey) {
+    key = mainKey;
+  }
   const collection = await phenomicFetch(createQuery(collectionConfig));
-  debug(route.path, `collection fetched. ${collection.list.length} items`);
+  debug(
+    route.path,
+    `collection fetched. ${collection.list.length} items (key: ${key})`
+  );
   const path = route.path || "*";
   const list = collection.list.reduce((acc, item) => {
     if (!item[key]) {
@@ -85,7 +94,7 @@ const resolveURLsForDynamicParams = async function(
     let resolvedPath = path.replace(":" + key, value);
     let params = { [key]: value };
     // try *
-    if (key === "key" && resolvedPath === path) {
+    if (key === mainKey && resolvedPath === path) {
       resolvedPath = resolvedPath.replace("*", value);
       params = { splat: value };
     }